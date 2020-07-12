4be722faf272460ad16935b6d6bab55e3ccc08dc
Brian Lerwick <lerwickbrianw@johndeere.com>
2020-07-12 06:44:09 -0500

    add level counter and comparison between computer and player selection

--- Project-1-Simon/js/script.js
+++ Project-1-Simon/js/script.js
@@ -1,6 +1,7 @@
 let gameSequence = []; // array for the random sequence
 let playerSequence = []; // array for the player sequence
 let gameActive = false; //varible to track if game is active
+let levelCounter = 0; //variable to track levels
 
 //HTML Selectors
 const startGameBtn = document.querySelector("#startGameBtn");
@@ -9,7 +10,7 @@ const redBtn = document.querySelector(".cell-red");
 const blueBtn = document.querySelector(".cell-blue");
 const yellowBtn = document.querySelector(".cell-yellow");
 const greenBtn = document.querySelector(".cell-green");
-
+const count = document.querySelector(".value");
 //Console lots for selector tests
 // console.log(startGameBtn);
 // console.log(endGameBtn);
@@ -17,6 +18,8 @@ const greenBtn = document.querySelector(".cell-green");
 // console.log(blueBtn);
 // console.log(yellowBtn);
 // console.log(greenBtn);
+
+count.innerHTML = levelCounter;
 
 //Event listener for Start Game button - executes playGame function
 startGameBtn.addEventListener("click", (event) => {
@@ -35,12 +38,9 @@ redBtn.addEventListener("click", (event) => {
     // push 1 into player sequence array
     playerSequence.push(1);
     console.log(playerSequence);
-    //call red button function
+    //call red button function to flash color
     red();
-    //return red button to unlit color
-    setTimeout(() => {
-      restoreColor();
-    }, 300);
+    checkMatch();
   }
 });
 // Event Listener for blue button
@@ -50,12 +50,9 @@ blueBtn.addEventListener("click", (event) => {
     // push 2 into player sequence array
     playerSequence.push(2);
     console.log(playerSequence);
-    //call blue button function
+    //call blue button function to flash color
     blue();
-    //return blue button to unlit color
-    setTimeout(() => {
-      restoreColor();
-    }, 300);
+    checkMatch();
   }
 });
 // Event Listener for yellow button
@@ -65,12 +62,9 @@ yellowBtn.addEventListener("click", (event) => {
     // push 3 into player sequence array
     playerSequence.push(3);
     console.log(playerSequence);
-    //call yellow button function
+    //call yellow button function to flash color
     yellow();
-    //return yellow button to unlit color
-    setTimeout(() => {
-      restoreColor();
-    }, 300);
+    checkMatch();
   }
 });
 // Event Listener for green button
@@ -80,45 +74,76 @@ greenBtn.addEventListener("click", (event) => {
     // push 4 into player sequence array
     playerSequence.push(4);
     console.log(playerSequence);
-    //call green button function
+    //call green button function to flash color
     green();
-    //return green button to unlit color
-    setTimeout(() => {
-      restoreColor();
-    }, 300);
+    checkMatch();
   }
 });
 
 //function for events when red button is pressed
 function red() {
   redBtn.style.backgroundColor = "rgb(248,1,1)";
+  //return red button to unlit color
+  setTimeout(() => {
+    restoreColor();
+  }, 300);
 }
 
 //function for events when blue button is pressed
 function blue() {
   blueBtn.style.backgroundColor = "rgb(1,1,250)";
+  //return blue button to unlit color
+  setTimeout(() => {
+    restoreColor();
+  }, 300);
 }
 
 //function for events when yellow button is pressed
 function yellow() {
   yellowBtn.style.backgroundColor = "rgb(255,255,1)";
+  //return yellow button to unlit color
+  setTimeout(() => {
+    restoreColor();
+  }, 300);
 }
 
 //function for events when green button is pressed
 function green() {
   greenBtn.style.backgroundColor = "rgb(1,255,1)";
+  //return green button to unlit color
+  setTimeout(() => {
+    restoreColor();
+  }, 300);
 }
+
 // playGame function - main function for game play
 function playGame() {
-  console.log("game started");
   generateSequence();
   console.log(gameSequence);
   gameActive = true;
+  levelCounter = 1;
+  count.innerHTML = levelCounter;
+  console.log(gameActive);
 
   //      Clear any stored sequences from previous game
   //  	Call random sequence generation
 }
 
+// check match function
+function checkMatch() {
+  if (gameSequence[levelCounter - 1] === playerSequence[levelCounter - 1]) {
+    console.log(
+      ` computer ${gameSequence[levelCounter - 1]} player ${
+        playerSequence[levelCounter - 1]
+      }`
+    );
+    levelCounter++;
+    count.innerHTML = levelCounter;
+    console.log("sequence matches");
+  } else {
+    console.log("incorrect pick");
+  }
+}
 // endGame function - handles Quit button function when player wants to quit
 function endGame() {
   console.log("game ended");
@@ -130,7 +155,7 @@ function endGame() {
 function restoreColor() {
   redBtn.style.backgroundColor = "darkred";
   blueBtn.style.backgroundColor = "darkblue";
-  yellowBtn.style.backgroundColor = "goldenrod";
+  yellowBtn.style.backgroundColor = "#ffd11c";
   greenBtn.style.backgroundColor = "darkgreen";
 }
 
